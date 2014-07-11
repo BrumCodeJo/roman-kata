154a21892b2e0201266449e6ada9c43635bbcedb
Jack Wearden <jack@jackwearden.co.uk>
2014-07-11 16:22:03 +0100

    extract mappings to class

--- romans.py
+++ romans.py
@@ -1,5 +1,25 @@
 class Romans:
 
+    roman_from_num = {
+        1: "I",
+        5: "V",
+        10: "X",
+        50: "L",
+        100: "C",
+        500: "D",
+        1000: "M"
+    }
+
+    num_from_roman = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+
     def __init__(self, value):
         if type(value) == str:
             self._str = value
@@ -16,17 +36,7 @@ class Romans:
 
     @staticmethod
     def str_to_num(value):
-        values = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000
-        }
-
-        result = [values.get(i) for i in value]
+        result = [num_from_roman.get(i) for i in value]
 
         for i in range(len(result)):
             if result[i] < result[min(len(result)-1, i+1)]:
@@ -36,20 +46,11 @@ class Romans:
 
     @staticmethod
     def num_to_str(value):
-        values = {
-            1: "I",
-            5: "V",
-            10: "X",
-            50: "L",
-            100: "C",
-            500: "D",
-            1000: "M"
-        }
         result = ""
-        for v in sorted(values.keys(), reverse=True):
+        for v in sorted(num_from_roman.keys(), reverse=True):
             unit_count = value // v
-            result += values[v] * unit_count
-            value -= unit_count * v
+            result += num_from_roman[v] * unit_count
+            value -= unit_count* v
 
         result = result.replace("VIIII", "IX").replace("LXXXX", "XC").replace("DCCCC", "CM")
         return result.replace("IIII", "IV").replace("XXXX", "XL").replace("CCCC", "CD")
